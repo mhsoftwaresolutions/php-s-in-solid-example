ba37e401d9e8a0ea1a07e31714e09b6436b4ec59
mhsoftwaresolutions <85870717+mhsoftwaresolutions@users.noreply.github.com>
2021-06-18 15:17:09 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * PHP Example of a the S in Solid Principles
+ * PHP Example of a the S in SOLID Principles
  * 
  * Single Responsibility Principle states:  A class should be 
  * responsible for only one thing.
